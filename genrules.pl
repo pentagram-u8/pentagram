6b49ce6b040b172b8a11bf35173b2c4fde736a3b
Patrick Burke <darkepaw@users.sourceforge.net>
2002-08-25 06:41:31 +0000

    *snarl* And the _other_ files I missed, when I committed the wrong tree. *sigh*

---
+++ genrules.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+#
+# This file is invoked by the Makefile and generates make rules
+# for end products (usually application binaries).
+# This currently only uses $(CXX) for the linking, but could be
+# customized for other things, too.
+#
+
+print "# This file is generated automatically by genrules.pl. Do no edit!\n\n";
+foreach my $product (@ARGV) {
+	# Generate the rules for each product
+	print $product.'_OBJ := $(patsubst %,$(LPATH)/%,$('.$product.'_OBJ))'."\n";
+	print $product.': $(LPATH)/'.$product.'$(EXEEXT)'."\n";
+	print '$(LPATH)/'.$product.'$(EXEEXT): $('.$product.'_OBJ)'."\n";
+	print '	$(CXX) -g -o $@ $+ $(LDFLAGS) $(SDL_LIBS)'."\n";
+	print "\n";
+}
---
+++ tools/module.mk
@@ -0,0 +1,9 @@
+# TODO - it would be nice if LPATH could be set by the Makefile that
+# includes us, since that has to now our path anyway.
+LPATH := tools
+
+LSRC := $(wildcard $(LPATH)/*.cc)
+LPRODUCTS :=
+
+# Common rules
+include common.mk
