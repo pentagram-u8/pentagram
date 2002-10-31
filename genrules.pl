58e1c41c739bb84f918bea8f84a04d83d2f9689f
Willem Jan Palenstijn <wjp@usecode.org>
2002-10-31 19:20:31 +0000

    2002-10-31  Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net> 	* various: added some .cvsignore files 	* bootstrap: generate Makefiles automatically with proper relative path 	* genrules.pl: fixed typo

--- genrules.pl
+++ genrules.pl
@@ -9,7 +9,7 @@ use strict;
 # customized for other things, too.
 #
 
-print "# This file is generated automatically by genrules.pl. Do no edit!\n\n";
+print "# This file is generated automatically by genrules.pl. Do not edit!\n\n";
 foreach my $product (@ARGV) {
 	# Generate the rules for each product
 	print $product.'_OBJ := $(patsubst %,$(LPATH)/%,$('.$product.'_OBJ))'."\n";
