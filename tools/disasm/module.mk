17d48da56865388a635749f1a29ba9ceac2ea9d2
Willem Jan Palenstijn <wjp@usecode.org>
2002-11-02 13:14:59 +0000

    2002-11-02  Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net> 	* tools/disasm/module.mk: removed weird -I flag 	* Makefile.in: we don't have headers/ anymore

--- tools/disasm/module.mk
+++ tools/disasm/module.mk
@@ -1,7 +1,6 @@
 # TODO - it would be nice if LPATH could be set by the Makefile that
 # includes us, since that has to know our path anyway.
 LPATH := tools/disasm
-CXXFLAGS += -I../../..
 
 LSRC := $(wildcard $(LPATH)/*.cpp)
 LPRODUCTS := disasm
