4c6de92787cd38d6c61a4fb031e553633ab96c23
Willem Jan Palenstijn <wjp@usecode.org>
2003-05-15 20:44:35 +0000

    typo

--- world/actors/ActorAnimProcess.h
+++ world/actors/ActorAnimProcess.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "Process.h"
 
 class Actor;
-class AnimAction;
+struct AnimAction;
 
 class ActorAnimProcess : public Process
 {
