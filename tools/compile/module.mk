a9a0e7902769a390c221e19f038a62045409bf73
Patrick Burke <darkepaw@users.sourceforge.net>
2003-05-28 13:05:26 +0000

    * configure.ac: Checks for `sed` and reports a warning if it's lacking. * tools/compile/module.mk: Runs a few sed scripts so that it shouldn't be necessary to hack-make a correct FlexLexer.h or prepend a #include "pent_include.h" * tools/compile/llcLexer.cpp: The updated 'should work' file. Of course this needs a little testing on a system that's just slightly more broken then mine. *grin*

--- tools/compile/module.mk
+++ tools/compile/module.mk
@@ -8,7 +8,17 @@ LPRODUCTS :=
 tools/compile/llcLexer.l: 
 
 tools/compile/llcLexer.cpp: tools/compile/llcLexer.l
-	flex++ -+ -otools/compile/llcLexer.cpp tools/compile/llcLexer.l
+	flex++ -+ -otools/compile/llcLexer.preproc1 tools/compile/llcLexer.l
+	# Fix the reference to <FlexLexer.h>...
+	sed 's/#include <FlexLexer.h>/#include "llcLexer.h"/' tools/compile/llcLexer.preproc1  >tools/compile/llcLexer.preproc2
+	# Fix the misnaming of the #line's...
+	sed 's/"tools\/compile\/llcLexer.preproc1"/"tools\/compile\/llcLexer.cpp"/' tools/compile/llcLexer.preproc2 >tools/compile/llcLexer.preproc3
+	# What the heck, lets fix a few of the warnings whilst we're at it!
+	sed 's/#define YY_SC_TO_UI(c) ((unsigned int) (unsigned char) c)/#define YY_SC_TO_UI(c) (static_cast<unsigned int>(static_cast<unsigned char>(c)))/' tools/compile/llcLexer.preproc3 >tools/compile/llcLexer.preproc4
+	# Add the necessary One True File
+	sed 's/llcFlexLexer/llcFlexLexer\n#include "pent_include.h"/' tools/compile/llcLexer.preproc4 >tools/compile/llcLexer.cpp
+	# Do a little cleanup
+	$(RM) tools/compile/llcLexer.preproc1 tools/compile/llcLexer.preproc2 tools/compile/llcLexer.preproc3 tools/compile/llcLexer.preproc4
 
 # Common rules
 include common.mk
