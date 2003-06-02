3572b5900199b88243a45618ad2aa7fcf0d2036c
Willem Jan Palenstijn <wjp@usecode.org>
2003-06-02 18:21:56 +0000

    removed strstream

--- conf/XMLTree.cpp
+++ conf/XMLTree.cpp
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "FileSystem.h"
 
 #include "util.h"
-#include <strstream>
 
 XMLTree::XMLTree()
 	: tree(new XMLNode("config")), root("config"), is_file(false),
