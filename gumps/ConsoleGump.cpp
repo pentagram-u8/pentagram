372e9d0940367df4a41c8b8df1bf94130cefbd9b
Willem Jan Palenstijn <wjp@usecode.org>
2004-01-25 21:28:10 +0000

    include GUIApp.h

--- gumps/ConsoleGump.cpp
+++ gumps/ConsoleGump.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2003  The Pentagram Team
+ *  Copyright (C) 2003-2004  The Pentagram Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,6 +20,8 @@
 #include "ConsoleGump.h"
 #include "IDataSource.h"
 #include "ODataSource.h"
+
+#include "GUIApp.h"
 
 DEFINE_RUNTIME_CLASSTYPE_CODE(ConsoleGump,Gump);
 
