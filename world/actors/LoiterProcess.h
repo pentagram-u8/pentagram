9a620762dfa96cf2db4db5091c666b66a42d67af
Willem Jan Palenstijn <wjp@usecode.org>
2004-11-21 19:19:24 +0000

    fixed copy-paste error

--- world/actors/LoiterProcess.h
+++ world/actors/LoiterProcess.h
@@ -17,7 +17,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #ifndef LOITERPROCESS_H
-#define DELETEACTORPROCESS_H
+#define LOITERPROCESS_H
 
 #include "Process.h"
 
