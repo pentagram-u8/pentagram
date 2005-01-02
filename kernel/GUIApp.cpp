c81db447455d17a2b17a49a6a1e9aa4c39bf2d7a
Willem Jan Palenstijn <wjp@usecode.org>
2005-01-02 15:50:50 +0000

    include ctime

--- kernel/GUIApp.cpp
+++ kernel/GUIApp.cpp
@@ -45,6 +45,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "SavegameWriter.h"
 #include "Savegame.h"
+#include <ctime>
 
 #include "Gump.h"
 #include "DesktopGump.h"
