be168d990d570f8ba000c58bf5e32d4c9e0051e1
Willem Jan Palenstijn <wjp@usecode.org>
2005-03-20 22:07:10 +0000

    missing headers

--- audio/AudioProcess.cpp
+++ audio/AudioProcess.cpp
@@ -26,6 +26,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "AudioSample.h"
 #include "AudioMixer.h"
 
+#include "IDataSource.h"
+#include "ODataSource.h"
+
 using Pentagram::AudioSample;
 using Pentagram::AudioMixer;
 
