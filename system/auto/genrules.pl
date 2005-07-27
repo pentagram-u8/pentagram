a122fdfe5e0f53c7998460e8ef9a4b7d66ff10d3
Willem Jan Palenstijn <wjp@usecode.org>
2005-07-27 21:43:04 +0000

    2005-07-27  Willem Jan Palenstijn <wjpalenstijn@users.sourceforge.net> 	* acinclude.m4: wrote custom SDL_ttf configure check PENT_CHECK_SDLTTF 	* configure.ac, common.mk, Makefile.in, */module.mk: 	  split targets into GUI and console apps. Don't link console apps   	  against SDL and audio libs.

--- system/auto/genrules.pl
+++ system/auto/genrules.pl
@@ -9,13 +9,33 @@ use strict;
 # customized for other things, too.
 #
 
+# passing '--gui' means all following products are built as full GUI apps
+# passing '--con' means all following products are built as console apps
+# (and they cancel eachother)
+
+my $gui = 1;
+
 print "# This file is generated automatically by genrules.pl. Do not edit!\n\n";
 foreach my $product (@ARGV) {
+	if ($product eq "--gui") {
+		$gui = 1;
+		next;
+	}
+	if ($product eq "--con") {
+		$gui = 0;
+		next;
+	}
+
 	# Generate the rules for each product
 	print 'ifneq ($(LPATH)$(EXEEXT),.)'."\n";
 	print $product.': $(LPATH)/'.$product.'$(EXEEXT)'."\n";
 	print "endif\n";
 	print '$(LPATH)/'.$product.'$(EXEEXT): $('.$product.'_OBJ)'."\n";
-	print '	$(CXX) -g -o $@ $+ $(LDFLAGS) $(SDL_LIBS)'."\n";
+	if ($gui == 1) {
+		print "\t".'$(CXX) -g -o $@ $+ $(LDFLAGS) $(SYS_LIBS) $(GUI_LIBS)'."\n";
+	} else {
+		print "\t".'$(CXX) -g -o $@ $+ $(LDFLAGS) $(SYS_LIBS) $(CON_LIBS)'."\n";
+	}
+
 	print "\n";
 }
