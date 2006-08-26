3e17f271248e401f942e79bd2a253b2d7db59281
Willem Jan Palenstijn <wjp@usecode.org>
2006-08-26 15:55:17 +0000

    whitespace

--- graphics/TexturePNG.cpp
+++ graphics/TexturePNG.cpp
@@ -47,25 +47,25 @@ bool TexturePNG::Read(IDataSource *ds)
 	if (!png_ptr)
 		return false;
 
-    png_infop info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr)
-    {
-        png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
-        return false;
-    }
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr)
+	{
+		png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+		return false;
+	}
 
-    png_infop end_info = png_create_info_struct(png_ptr);
-    if (!end_info)
-    {
-        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-        return false;
-    }
+	png_infop end_info = png_create_info_struct(png_ptr);
+	if (!end_info)
+	{
+		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+		return false;
+	}
 
-    if (setjmp(png_jmpbuf(png_ptr))) {
+	if (setjmp(png_jmpbuf(png_ptr))) {
 		delete[] row_pointers;
-        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-        return false;
-    }
+		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+		return false;
+	}
 
 	png_set_read_fn(png_ptr, ds, idatasource_png_read_data);
 	png_set_sig_bytes(png_ptr, 8);
@@ -78,68 +78,68 @@ bool TexturePNG::Read(IDataSource *ds)
 	png_get_IHDR(png_ptr, info_ptr, &width, &height,
 				 &bit_depth, &color_type,
 				 &interlace_type, &compression_type, &filter_method);
- 
-	if (color_type == PNG_COLOR_TYPE_PALETTE)
-        png_set_palette_to_rgb(png_ptr);
 
-    if (color_type == PNG_COLOR_TYPE_GRAY ||
-			color_type == PNG_COLOR_TYPE_RGB_ALPHA)
+	if (color_type == PNG_COLOR_TYPE_PALETTE)
+		png_set_palette_to_rgb(png_ptr);
+
+	if (color_type == PNG_COLOR_TYPE_GRAY || 
+		color_type == PNG_COLOR_TYPE_RGB_ALPHA)
 		png_set_gray_to_rgb(png_ptr);
 
-    if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
+	if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
 		png_set_tRNS_to_alpha(png_ptr);
 
-    if (bit_depth == 16)
-        png_set_strip_16(png_ptr);
+	if (bit_depth == 16)
+		png_set_strip_16(png_ptr);
 
-   if (color_type == PNG_COLOR_TYPE_RGB)
-	   png_set_filler(png_ptr, 0xFF, PNG_FILLER_AFTER);
+	if (color_type == PNG_COLOR_TYPE_RGB)
+		png_set_filler(png_ptr, 0xFF, PNG_FILLER_AFTER);
 
-   png_read_update_info(png_ptr, info_ptr);
+	png_read_update_info(png_ptr, info_ptr);
 
-   png_uint_32 rowbytes = png_get_rowbytes(png_ptr, info_ptr);
-   if (rowbytes != width * 4) {
-	   perr << "TexturePNG::Read(): failed to convert PNG to 32 bit RGBA"
-			<< std::endl;
-       png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-	   return false;
-   }
+	png_uint_32 rowbytes = png_get_rowbytes(png_ptr, info_ptr);
+	if (rowbytes != width * 4) {
+		perr << "TexturePNG::Read(): failed to convert PNG to 32 bit RGBA"
+			 << std::endl;
+		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+		return false;
+	}
 
-   buffer = new uint32[width*height];
-   if (!buffer) {
-       png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-	   return false;	   
-   }
+	buffer = new uint32[width*height];
+	if (!buffer) {
+		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+		return false;	   
+	}
 
-   row_pointers = new png_bytep[height];
-   for (unsigned int i = 0; i < height; ++i) {
-	   row_pointers[i] = reinterpret_cast<png_bytep>(&buffer[i*width]);
-   }
+	row_pointers = new png_bytep[height];
+	for (unsigned int i = 0; i < height; ++i) {
+		row_pointers[i] = reinterpret_cast<png_bytep>(&buffer[i*width]);
+	}
 
-   png_read_image(png_ptr, row_pointers);
+	png_read_image(png_ptr, row_pointers);
 
-   delete[] row_pointers;
-   row_pointers = 0;
+	delete[] row_pointers;
+	row_pointers = 0;
 
-   // the end_info struct isn't used, but passing it anyway for now
-   png_read_end(png_ptr, end_info);
+	// the end_info struct isn't used, but passing it anyway for now
+	png_read_end(png_ptr, end_info);
 
-   png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+	png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
 
-   this->width = width;
-   this->height = height;
-   this->format = TEX_FMT_STANDARD;
-   this->wlog2 = -1;
-   this->hlog2 = -1;
+	this->width = width;
+	this->height = height;
+	this->format = TEX_FMT_STANDARD;
+	this->wlog2 = -1;
+	this->hlog2 = -1;
 
-   // Repack RGBA
-   for (unsigned int i = 0; i < height * width; ++i) {
-       png_bytep p = reinterpret_cast<png_bytep>(&buffer[i]);
-       buffer[i] =(p[0] << TEX32_R_SHIFT) 
-                | (p[1] << TEX32_G_SHIFT)
-                | (p[2] << TEX32_B_SHIFT)
-                | (p[3] << TEX32_A_SHIFT);
-   }
-
-   return true;
+	// Repack RGBA
+	for (unsigned int i = 0; i < height * width; ++i) {
+		png_bytep p = reinterpret_cast<png_bytep>(&buffer[i]);
+		buffer[i] =(p[0] << TEX32_R_SHIFT) 
+			| (p[1] << TEX32_G_SHIFT)
+			| (p[2] << TEX32_B_SHIFT)
+			| (p[3] << TEX32_A_SHIFT);
+	}
+	
+	return true;
 }
