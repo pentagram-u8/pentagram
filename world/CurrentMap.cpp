e24f4abc9341029a4f8edf6f8bd534bff5392644
Willem Jan Palenstijn <wjp@usecode.org>
2008-11-04 22:34:46 +0000

    include climits for gcc 4.3

--- world/CurrentMap.cpp
+++ world/CurrentMap.cpp
@@ -18,6 +18,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "pent_include.h"
 
+#include <climits>
+
 #include "CurrentMap.h"
 #include "Map.h"
 #include "Item.h"
