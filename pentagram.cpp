c66d9021ed061b2795270502cb27ca3fe0b99669
Ryan Nunn <colourles@users.sourceforge.net>
2009-09-02 10:32:54 +0000

    Removed dependency of the SDLmain.lib file for windows builds. The code in the supplied WinMain function will redirect stdout and stderr to text files like SDL would normally do. This code needs to be fixed so its multiuser aware for Vista/7

--- pentagram.cpp
+++ pentagram.cpp
@@ -23,6 +23,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "MemoryManager.h"
 #include "version.h"
 
+#ifdef main
+#undef main
+#endif
+
 int main(int argc, char* argv[])
 {
 	// Initialize Memory Manager here to avoid extra tools depending on it
@@ -48,3 +52,117 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
+#ifdef _WIN32
+#include <windows.h>
+#include <cstdio>
+
+ PCHAR*
+ CommandLineToArgvA(
+     PCHAR CmdLine,
+     int* _argc
+     )
+ {
+     PCHAR* argv;
+     PCHAR  _argv;
+     ULONG   len;
+     ULONG   argc;
+     CHAR   a;
+     ULONG   i, j;
+
+     BOOLEAN  in_QM;
+     BOOLEAN  in_TEXT;
+     BOOLEAN  in_SPACE;
+
+     len = strlen(CmdLine);
+     i = ((len+2)/2)*sizeof(PVOID) + sizeof(PVOID);
+
+     argv = (PCHAR*)GlobalAlloc(GMEM_FIXED,
+         i + (len+2)*sizeof(CHAR));
+
+     _argv = (PCHAR)(((PUCHAR)argv)+i);
+
+     argc = 0;
+     argv[argc] = _argv;
+     in_QM = FALSE;
+     in_TEXT = FALSE;
+     in_SPACE = TRUE;
+     i = 0;
+     j = 0;
+
+     while( a = CmdLine[i] ) {
+         if(in_QM) {
+             if(a == '\"') {
+                 in_QM = FALSE;
+             } else {
+                 _argv[j] = a;
+                 j++;
+             }
+         } else {
+             switch(a) {
+             case '\"':
+                 in_QM = TRUE;
+                 in_TEXT = TRUE;
+                 if(in_SPACE) {
+                     argv[argc] = _argv+j;
+                     argc++;
+                 }
+                 in_SPACE = FALSE;
+                 break;
+             case ' ':
+             case '\t':
+             case '\n':
+             case '\r':
+                 if(in_TEXT) {
+                     _argv[j] = '\0';
+                     j++;
+                 }
+                 in_TEXT = FALSE;
+                 in_SPACE = TRUE;
+                 break;
+             default:
+                 in_TEXT = TRUE;
+                 if(in_SPACE) {
+                     argv[argc] = _argv+j;
+                     argc++;
+                 }
+                 _argv[j] = a;
+                 j++;
+                 in_SPACE = FALSE;
+                 break;
+             }
+         }
+         i++;
+     }
+     _argv[j] = '\0';
+     argv[argc] = NULL;
+
+     (*_argc) = argc;
+     return argv;
+ }
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	int argc;
+	char **argv = CommandLineToArgvA(GetCommandLineA(), &argc);
+
+	// FIXME - Multiuser aware please
+
+	std::freopen("pstdout.txt","wt", stdout);
+	std::freopen("pstderr.txt","wt", stderr);
+
+	int res = main(argc, argv);
+
+	if (!std::ftell(stdout)) {
+		std::fclose(stdout);
+		std::remove("pstdio.txt");
+	}
+	if (!std::ftell(stderr)) {
+		std::fclose(stderr);
+		std::remove("pstderr.txt");
+	}
+
+	return  res;
+}
+#endif
