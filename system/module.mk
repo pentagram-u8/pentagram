739352102231b7ab3a9d00ceaf73d624553aab54
Willem Jan Palenstijn <wjp@usecode.org>
2010-10-28 17:32:28 +0000

    Fix copy/paste error

--- system/module.mk
+++ system/module.mk
@@ -1,6 +1,6 @@
 # TODO - it would be nice if LPATH could be set by the Makefile that
 # includes us, since that has to know our path anyway.
-LPATH := tools
+LPATH := system
 
 LSRC := $(wildcard $(srcdir)/$(LPATH)/*.cpp)
 
